0f89accc38e2e8415618dbcc21e976c6cc71a895
Privatik3 <lol11999333@gmail.com>
2018-06-02 00:51:36 +0300

    Добавил вывод сток статуса

--- catalog/controller/product/category.php
+++ catalog/controller/product/category.php
@@ -212,6 +212,7 @@ class ControllerProductCategory extends Controller {
 
 				$data['products'][] = array(
 					'product_id'  => $result['product_id'],
+                    'stock_status'  => $result['stock_status'],
 					'thumb'       => $image,
 					'name'        => $result['name'],
 					'description' => utf8_substr(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8')), 0, $this->config->get('config_product_description_length')) . '..',
